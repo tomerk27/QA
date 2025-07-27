ac55c2cfb80c863624eb8757910f36a47350f32f
Tomer Kaplan <tomerkaplan@Tomers-MacBook-Pro.local>
2025-07-27 12:30:42 +0300

    add new exercise

--- playwright_tests/tests/example.spec.js
+++ playwright_tests/tests/example.spec.js
@@ -1,15 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('login and out', async ({page}) => {
-  await page.goto('https://dev-admin.funzy.world/');
-  await page.getByRole('textbox', {name: 'email'}).fill('admin@funzy-world.com');
-  await page.getByRole('textbox', {name: 'password'}).fill('funzy-world123456');
-  await page.getByRole('button', {name: 'login'}).click()
-  await page.getByRole('button', { name: 'account of current user' }).nth(1).click();
-  await page.getByRole('menuitem', { name: 'Logout' }).click();
-  await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
-});
-
 test( 'exercise', async ({page}) => {
   await page.goto ('https://demoqa.com/text-box')
   
@@ -22,4 +12,29 @@ test( 'exercise', async ({page}) => {
   await page.locator('#permanentAddress').click();
   await page.locator('#permanentAddress').fill('none');
   await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.getByText('Name:Tomer KaplanEmail:tomer.')).toBeVisible()
+});
+
+test( 'Practice Form', async ({ page }) => {
+  await page.goto('https://demoqa.com/automation-practice-form');
+
+  await page.getByRole('textbox', {name: 'First Name'}).fill('Tomer');
+  await page.getByRole('textbox', {name: 'Last Name'}).fill('Kaplan');
+  await page.getByRole('textbox', {name: 'name@example.com'}).fill('tomer.kaplan2007@gmail.com');
+  await page.getByText('Male', { exact: 'true'});
+  await page.locator('div').filter({ hasText: /^Male$/ }).click();
+  await page.getByRole('textbox', {name: 'Mobile Number'}).fill('0542668824');
+  await page.locator('#dateOfBirthInput').fill('27 Nov 2007');
+  await page.locator('#subjectsInput').fill('bla bla bla');
+  await page.getByText('Sports').click();
+  await page.getByText('Music').click();
+  await page.getByRole('checkbox', {name: 'music'}).check();
+  await page.getByRole('button', { name: 'submit'}).click();
+  await expect(page.getByText('Thanks for submitting the form')).toBeVisible()
+});
+
+test( 'slider test', async ({ page }) => {
+  await page.goto('https://demoqa.com/slider')
+  await page.getByRole('slider').fill('27');
+  await expect(page.locator('#sliderValue')).toHaveValue('27');
 });
